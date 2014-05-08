035d5989bb885b336bafaf8f1eed85e5b8f08425
Mikael Östberg <mikael.ostberg@smartbear.com>
2014-05-08 14:34:40 +0200

    SLT-572 Virtualhost fix for new ops-deployed rabbitmq

--- jcelery/src/main/java/org/loadui/jcelery/base/RabbitProvider.java
+++ jcelery/src/main/java/org/loadui/jcelery/base/RabbitProvider.java
@@ -3,13 +3,16 @@ package org.loadui.jcelery.base;
 import com.rabbitmq.client.ConnectionFactory;
 import org.loadui.jcelery.ConnectionProvider;
 
+import static com.google.common.base.Objects.firstNonNull;
+
 public class RabbitProvider implements ConnectionProvider
 {
 	private ConnectionFactory connectionFactory;
 
-	public RabbitProvider( String host, int port, String username, String password  )
+	public RabbitProvider( String host, int port, String username, String password, String vhost  )
 	{
 		this.connectionFactory = new ConnectionFactory();
+		this.connectionFactory.setVirtualHost( vhost );
 		this.connectionFactory.setHost( host );
 		this.connectionFactory.setPort( port );
 		this.connectionFactory.setUsername( username );
