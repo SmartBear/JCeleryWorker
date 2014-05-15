e285b92daa9e0f04a89de5297d0eb41599bc9da5
Renato Athaydes <renato.deathaydes@smartbear.com>
2014-05-15 12:49:36 +0200

    Just testing jenkins

--- jcelery-test/src/test/java/org/loadui/jcelery/JobServiceTest.java
+++ jcelery-test/src/test/java/org/loadui/jcelery/JobServiceTest.java
@@ -40,7 +40,6 @@ public class JobServiceTest
 	public Option[] config()
 	{
 		return options(
-				repository( "http://nexus.sthlm.smartbear.local:8081/content/repositories/next" ).id( "next" ),
 				systemPackages(
 						"com.sun.crypto.provider",
 						"com.sun.net.ssl",
